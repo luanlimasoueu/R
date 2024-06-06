2c51f83d8343a5aaa30308c126354ac4d5ed7402
okluan <luanemerson8@gmail.com>
2024-06-06 18:08:57 -0300

    R

--- 1_exemplo/1_exemplo/1_exemplo.R
+++ 1_exemplo/1_exemplo/1_exemplo.R
@@ -1,2 +1,14 @@
-a <- 2
-print(a)
+# geeksforgeeks
+
+# Assigning values to variables
+a <- 9
+b <- 4
+
+# Printing sum
+print(a + b)
+
+# The following code will be executed
+x <- 10
+y <- 20
+sum <- x + y
+print(sum) # This line will print the value of 'sum'
