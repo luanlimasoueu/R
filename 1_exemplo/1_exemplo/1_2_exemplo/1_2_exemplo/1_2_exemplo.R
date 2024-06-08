9e8d2d48c13414858aca8676f13c93fc4d5b8632
okluan <luanemerson8@gmail.com>
2024-06-07 23:26:51 -0300

    R

---
+++ 1_exemplo/1_exemplo/1_2_exemplo/1_2_exemplo/1_2_exemplo.R
@@ -0,0 +1,16 @@
+#Variáveis
+
+# R program to illustrate
+# Initialization of variables
+
+# using equal to operator
+var1 = "hello"
+print(var1)
+
+# using leftward operator
+var2 <- "hello2"
+print(var2)
+
+# using rightward operator
+"hello" -> var3
+print(var3)
