bd9bbe51601c1b35fb2c84aee9c3dbca0ac2f2de
okluan <luanemerson8@gmail.com>
2024-06-07 23:28:40 -0300

    R

--- 1_exemplo/1_exemplo/1_2_exemplo/1_2_exemplo/1_2_exemplo.R
+++ 1_exemplo/1_exemplo/1_2_exemplo/1_2_exemplo/1_2_exemplo.R
@@ -14,3 +14,38 @@ print(var2)
 # using rightward operator
 "hello" -> var3
 print(var3)
+
+
+# R program to illustrate
+# usage of global variables
+
+# global variable
+global = 5
+
+# global variable accessed from
+# within a function
+display = function(){
+  print(global)
+}
+display()
+
+# changing value of global variable
+global = 10
+display()
+
+
+# R program to illustrate
+# usage of local variables
+
+func = function(){
+  # this variable is local to the
+  # function func() and cannot be
+  # accessed outside this function
+  age = 18    
+  print(age)
+}
+
+cat("Age is:\n")
+func()
+
+
