14da89502eb43b808478d697f5314d5951842106
okluan <luanemerson8@gmail.com>
2024-06-09 17:05:37 -0300

    R

--- 2_exemplo/2_3_exemplo/2_exemplo.R
+++ 2_exemplo/2_3_exemplo/2_exemplo.R
@@ -2,3 +2,33 @@
 vetor1 <- c(1, 5, 3, -10)
 vetor2 <- c("a", "b", "c")
 print(vetor2)
+
+vetor <- c("a", "b", "c", "d")
+vetor[1]
+
+vetor[c(2, 3)]
+
+vetor1 <- c(1, 5, 3, -10)
+vetor2 <- c("a", "b", "c")
+
+class(vetor1)
+class(vetor2)
+
+
+vetor <- c(0, 5, 20, -3)
+vetor + 1
+
+
+vetor1 <- c(1, 2, 3)
+vetor2 <- c(10, 20, 30)
+
+vetor1  + vetor2
+
+
+vetor1 <- c(1, 2)
+vetor2 <- c(10, 20, 30, 40)
+
+vetor1 + vetor2
+
+
+
---
+++ 2_exemplo/2_4_exemplo/2_4_exemplo.R
@@ -0,0 +1,37 @@
+class(TRUE)
+
+class(FALSE)
+
+1 == 1
+
+"a" == "b"
+
+3 > 3
+
+
+minha_coluna <- c(1, 3, 0, 10, -1, 5, 20)
+minha_coluna > 3
+
+#Valores Especiais
+
+vetor <- c(1, 2, 3)
+vetor[4]
+
+
+idade_ana <- 30
+idade_beto <- NA
+idade_carla <- NA
+
+idade_ana == idade_beto
+
+is.na(NA)
+
+
+10^310
+
+
+
+
+
+
+
