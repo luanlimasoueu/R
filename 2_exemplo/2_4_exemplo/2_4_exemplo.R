f85417f1af2dcf95d661f9541ddf464484b6c2ea
okluan <luanemerson8@gmail.com>
2024-06-09 17:33:31 -0300

    R classe dataframe

--- 2_exemplo/2_4_exemplo/2_4_exemplo.R
+++ 2_exemplo/2_4_exemplo/2_4_exemplo.R
@@ -29,6 +29,58 @@ is.na(NA)
 
 10^310
 
+nao_sou_um_numero <- NaN
+objeto_infinito <- Inf
+objeto_nulo <- NULL
+
+is.nan(nao_sou_um_numero)
+
+
+is.infinite(objeto_infinito)
+
+
+is.null(objeto_nulo)
+
+
+#Listas
+list(1, "a", TRUE)
+
+lista <- list(1, "a", TRUE)
+
+lista[1]
+
+
+dados_cliente <- list(cliente = "Ana Silva", idade = 25, estado_civil = NA)
+
+dados_cliente
+
+dados_cliente$cliente
+
+class(dados_cliente[1])
+
+class(dados_cliente$cliente)
+
+#Dataframes
+
+as.list(mtcars)
+
+mtcars$mpg
+
+#Exemplo
+
+dados_cliente <- list(
+  cliente = c("Ana Silva", "Beto Pereira", "Carla Souza"),
+  idade = c(25, 30, 23),
+  estado_civil = c(NA, "Solteiro", "Casada")
+)
+
+dados_cliente 
+
+
+
+
+
+
 
 
 
