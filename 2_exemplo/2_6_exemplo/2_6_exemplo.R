5a5e60de16f9847a4825747a74789c57d0291791
okluan <luanemerson8@gmail.com>
2024-06-10 13:25:14 -0300

    R classe dataframe

--- 2_exemplo/2_6_exemplo/2_6_exemplo.R
+++ 2_exemplo/2_6_exemplo/2_6_exemplo.R
@@ -1,3 +1,78 @@
 library(readr)
 
 cars_csv <- read_csv(file = "cars_data.csv")
+
+install.packages("ggplot2")
+library(ggplot2)
+library(readr)
+
+imdb <- readr::read_rds("imdb.rds")
+ggplot(data = imdb)
+head(imdb)
+
+ggplot(imdb) +
+  geom_point(mapping = aes(x = orcamento, y = receita))
+
+ggplot(imdb) +
+  geom_point(mapping = aes(x = orcamento, y = receita)) +
+  geom_abline(intercept = 0, slope = 1, color = "red")
+
+
+ggplot(imdb) +
+  geom_abline(intercept = 0, slope = 1, color = "red") +
+  geom_point(mapping = aes(x = orcamento, y = receita)) 
+
+
+library(dplyr, warn.conflicts = FALSE)
+
+imdb %>%
+  mutate(lucro = receita - orcamento) %>% 
+  ggplot() +
+  geom_point(aes(x = orcamento, y = receita, color = lucro))
+
+
+imdb %>% 
+  group_by(ano) %>% 
+  summarise(nota_media = mean(nota_imdb, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = ano, y = nota_media))
+
+
+imdb %>% 
+  filter(diretor == "Steven Spielberg") %>% 
+  group_by(ano) %>% 
+  summarise(nota_media = mean(nota_imdb, na.rm = TRUE)) %>% 
+  mutate(nota_media = round(nota_media, 1)) %>% 
+  ggplot(aes(x = ano, y = nota_media)) +
+  geom_line() +
+  geom_label(aes(label = nota_media))
+
+
+imdb %>% 
+  count(diretor) %>%
+  slice_max(order_by = n, n = 10) %>% 
+  ggplot() +
+  geom_col(aes(x = direcao, y = n))
+
+head(mtcars)
+
+imdb %>% 
+  filter(diretor == "Steven Spielberg") %>%
+  group_by(ano) %>% 
+  summarise(nota_media = mean(nota_imdb, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = ano, y = nota_media)) +
+  geom_point(aes(x = ano, y = nota_media))
+
+imdb %>% 
+  filter(direcao == "Steven Spielberg") %>% 
+  group_by(ano) %>% 
+  summarise(nota_media = mean(nota_imdb, na.rm = TRUE)) %>% 
+  ggplot(aes(x = ano, y = nota_media)) +
+  geom_line() +
+  geom_point()
+
+
+
+
+
