a3d03db2207fb8728fb50e09aa73c4735c01b002
okluan <luanemerson8@gmail.com>
2025-05-19 23:59:46 -0300

    Study

--- RNotesforProfessionals/8_exemplo.R
+++ RNotesforProfessionals/8_exemplo.R
@@ -3,3 +3,29 @@ class(iris)
 str(iris)
 
 class(iris$Species)
+
+x <- as.Date("1970-01-01")
+
+class(x)
+
+mylist <- list( A = c(5,6,7,8), B = letters[1:10], CC = list( 5, "Z") )
+
+mylist
+
+f <- function(x) list(xplus = x + 10, xsq = x^2)
+
+f(7)
+
+c(1, 2, 3)
+
+c(TRUE, TRUE, FALSE)
+
+c("a", "b", "c")
+
+x <- c(1, 2, 5)
+y <- c(3, 4, 6)
+z <- c(x, y)
+z
+
+
+
