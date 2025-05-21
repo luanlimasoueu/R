a4a9c36c86302c0e469c336ad2afd4958ab3bae2
okluan <luanemerson8@gmail.com>
2025-05-20 23:45:17 -0300

    Study

---
+++ RNotesforProfessionals/9_capitulo.R
@@ -0,0 +1,17 @@
+l1 <- list(c(1, 2, 3), c("a", "b", "c"))
+
+l1
+
+names(l1)
+
+names(l1) <- c("vector1", "vector2")
+
+l2 <- list(vec = c(1, 3, 5, 7, 9),
+           mat = matrix(data = c(1, 2, 3), nrow = 3))
+l2
+
+exampleList1 <- list('a', 'b')
+exampleList2 <- list(1, 2)
+exampleList3 <- list('a', 1, 2)
+
+exampleList1
