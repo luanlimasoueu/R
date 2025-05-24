4fc27baf412887a0e60ae1ea8b7cc8e5fd17c411
okluan <luanemerson8@gmail.com>
2025-05-24 19:44:07 -0300

    Study

--- RNotesforProfessionals/11_exemplo.R
+++ RNotesforProfessionals/11_exemplo.R
@@ -2,3 +2,21 @@ letters
 
 LETTERS[7:9]
 
+letters[c(1,5,3,2,4)]
+
+
+xl <- list('a' = 5, 'b' = 6, 'c' = 7, 'd' = 8)
+
+xl
+
+x <- 5:8
+y <- letters[1:4]
+xy <- setNames(x, y)
+xy
+
+xy <- 5:8
+names(xy) <- letters[1:4]
+names(xy)
+
+mydf <- data.frame(let = c('c','a','b','d'))
+mydf
